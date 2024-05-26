26fca7dcbb8d9b971daa6425066a374814cfc884
K G I Tharudinitha <121121883+IsinduTharudinitha@users.noreply.github.com>
2024-05-26 05:04:26 -0700

    Add files via upload

---
+++ studentsystem/src/main/java/com/arjuncodes/studentsystem/controller/StudentController.java
@@ -0,0 +1,29 @@
+package com.arjuncodes.studentsystem.controller;
+
+import java.util.List;
+import com.arjuncodes.studentsystem.model.Student;
+import com.arjuncodes.studentsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/student")
+@CrossOrigin
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New student is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+}
+
